e10afbf6f250f1d6b937bf160d15a7de2b530fd7
EmirAkagunduz16 <akagunduzemir04@gmail.com>
2026-08-31 14:59:14 +0300

    some lint error

--- server/api/events/[id].get.ts
+++ server/api/events/[id].get.ts
@@ -1,7 +1,7 @@
 // GET /api/events/:id
 // Fetch a single event by Ticketmaster ID (includes embedded venues & attractions)
 
-import type { TmEvent } from '~/types/event'
+import type { TmEvent } from '~~/types/event'
 
 export default defineEventHandler(async (event) => {
   const id = getRouterParam(event, 'id')
